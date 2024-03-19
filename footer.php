9cd5dc7fa57d8a3a7c8e824abe5c0c974ab46b2e
Humam <humamasathin234@gmail.com>
2024-03-19 22:24:47 +0700

    update footer|belum responsif

--- footer.php
+++ footer.php
@@ -1,52 +1,46 @@
 <footer id="footer" class="section-bg">
     <div class="footer-top">
       <div class="container">
-
         <div class="row">
-
           <div class="col-lg-6">
-
             <div class="row">
-
               <div class="col-sm-2">
                   <img class="img-fluid" src="assets/img/logo.png" alt= "">
               </div>
-              <div class="col-sm-10">
-                
+              <div class="col-sm-10">  
                 <div class="footer-info">
                   <h3>SMK ASY-SYAMSURIYYAH</h3>
-                  <p>Cras fermentum odio eu feugiat lide par naso tierra. Justo eget nada terra videa magna derita valies darta donna mare fermentum iaculis eu non diam phasellus. Scelerisque felis imperdiet proin fermentum leo. Amet volutpat consequat mauris nunc congue.</p>
+                  <h4>"Bertaqwa, Berkarakter, dan Mandiri"</h4>
                 </div>
-
-                <div class="footer-newsletter">
+                <!-- <div class="footer-newsletter">
                   <h4>Our Newsletter</h4>
                   <p>Tamen quem nulla quae legam multos aute sint culpa legam noster magna veniam enim veniam illum dolore legam minim quorum culpa amet magna export quem.</p>
                   <form action="" method="post">
                     <input type="email" name="email"><input type="submit" value="Subscribe">
                   </form>
-                </div>
-
+                </div> -->
               </div>
 
-              <div class="col-sm-6">
+              <div style="margin-left: 110px;" class="col-sm-5">
                 <div class="footer-links">
-                  <h4>Useful Links</h4>
+                  <h4>Menu Utama</h4>
                   <ul>
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#about">About us</a></li>
-                    <li><a href="#">Services</a></li>
-                    <li><a href="#">Terms of service</a></li>
-                    <li><a href="#">Privacy policy</a></li>
+                    <li><a href="#">Beranda</a></li>
+                    <li><a href="#about">Tentang Kami</a></li>
+                    <li><a href="#">Program</a></li>
+                    <li><a href="#">Alumni</a></li>
+                    <li><a href="#">Hubungi Kami</a></li>
                   </ul>
                 </div>
-
+              </div>
+              <div class="col-sm-5">
                 <div class="footer-links">
-                  <h4>Contact Us</h4>
+                  <h4>Hubungi Kami</h4>
                   <p>
-                    A108 Adam Street <br>
-                    New York, NY 535022<br>
-                    United States <br>
-                    <strong>Phone:</strong> +1 5589 55488 55<br>
+                    Lempeni, Jagalempeni<br>
+                    Kec. Wanasari, Kab. Brebes<br>
+                    Jawa Tengah<br>
+                    <strong>Phone:</strong> +62 815 5588 8344<br>
                     <strong>Email:</strong> info@example.com<br>
                   </p>
                 </div>
@@ -65,8 +59,8 @@
           </div>
 
           <div class="col-lg-6">
-
-            </div>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3960.5923100716936!2d109.00992827509046!3d-6.93923026792911!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e6fbb467f961021%3A0xfa9b8da11e755086!2sSMK%20As-Syamsuriyyah!5e0!3m2!1sid!2sid!4v1710861117389!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+          </div>
 
           </div>
 
