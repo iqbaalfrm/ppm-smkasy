50822bdbe45250317bb060d975d283e5e589821b
Humam <humamasathin234@gmail.com>
2024-03-24 20:53:49 +0700

    add section berita

---
+++ sections/berita.php
@@ -0,0 +1,86 @@
+<section id="berita" class="berita">
+<!--kolom kategori-->
+    <div class="col-md-4 col-12">
+        <div class="sidbar-portfolio">
+            <div class="sidbar-portfolio-inner">
+                <h3>Kategori Berita</h3>
+                <div class="tag">
+                    <ul>
+                        <li><span class="float-left">Informasi Umum</span></li>
+                        <li><span class="float-left">Prestasi</span></li>
+                        <li><span class="float-left">Agenda Sekolah</span></li>
+                        <li><span class="float-left">Pengumuman</span></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--kolom berita-->
+    <div class="col-md-8 col-12">
+        <div class="row">
+            <!--per konten-->
+            <div class="col-lg-6 col-md-6">
+                <div class="project_product">
+                    <div class="project-holder">
+                        <a href="#">
+                            <img src=".../assets/img/intro-bg.jpg" alt="contoh">
+                            <div class="text-holder">
+                                <div class="text-holder-outer">
+                                    <div class="text-holder-inner">
+                                        <h4>SMK Asy-syamsuriyyah menang.</h4>
+                                        <span>
+                                            <i class="fa fa-calendar"></i>
+                                            1 Maret 2024
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-6 col-md-6">
+                <div class="project_product">
+                    <div class="project-holder">
+                        <a href="#">
+                            <img src=".../assets/img/intro-bg.jpg" alt="contoh">
+                            <div class="text-holder">
+                                <div class="text-holder-outer">
+                                    <div class="text-holder-inner">
+                                        <h4>SMK Asy-syamsuriyyah menang.</h4>
+                                        <span>
+                                            <i class="fa fa-calendar"></i>
+                                            1 Maret 2024
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-6 col-md-6">
+                <div class="project_product">
+                    <div class="project-holder">
+                        <a href="#">
+                            <img src=".../assets/img/intro-bg.jpg" alt="contoh">
+                            <div class="text-holder">
+                                <div class="text-holder-outer">
+                                    <div class="text-holder-inner">
+                                        <h4>SMK Asy-syamsuriyyah menang.</h4>
+                                        <span>
+                                            <i class="fa fa-calendar"></i>
+                                            1 Maret 2024
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
