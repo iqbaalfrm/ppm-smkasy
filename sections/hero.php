e8bbe7a57e0007047f174aa5a01f06459c4edf7d
Moch. Iqbal Firmansyah <65177974+iqbaalfrm@users.noreply.github.com>
2024-03-25 15:33:17 +0700

    Update hero.php

--- sections/hero.php
+++ sections/hero.php
@@ -1,17 +1,16 @@
 <section id="hero" class="clearfix">
     <div class="container d-flex h-100">
-      <div class="row justify-content-center align-self-center" data-aos="fade-up">
-        <div class="col-lg-6 intro-info order-lg-first order-last" data-aos="zoom-in" data-aos-delay="100">
-          <h2>“Bertaqwa, Berkarakter, dan Mandiri”</h2>
-          <div>
-            <a href="#call-to-action" class="btn-get-started scrollto">Daftar Sekarang!</a>
-          </div>
-        </div>
+        <div class="row justify-content-center align-items-center" data-aos="fade-up">
+            <div class="col-lg-6 intro-info order-lg-first order-last" data-aos="zoom-in" data-aos-delay="100"  style="margin-top: -125px;">
+                <h2>“Bertaqwa, Berkarakter, dan Mandiri”</h2>
+                <div>
+                    <a href="#call-to-action" class="btn-get-started scrollto">Daftar Sekarang!</a>
+                </div>
+            </div>
 
-        <div class="col-lg-6 intro-img order-lg-last order-first" data-aos="zoom-out" data-aos-delay="200">
-          <img src="assets/img/Humam.png" alt="" class="img-fluid">
+            <div class="col-lg-6 intro-img order-lg-last order-first text-center" data-aos="zoom-out" data-aos-delay="200" style="margin-top: -125px;">
+                <img src="assets/img/Humam.png" alt="" class="img-fluid" style="height: 450px;">
+            </div>
         </div>
-      </div>
-
     </div>
-  </section>
+</section>
