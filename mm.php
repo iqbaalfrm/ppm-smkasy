91110ec1edd949071f598624ac3b6fc318288761
berlin <berlianirsq@gmail.com>
2024-03-25 11:48:00 +0700

    nambah sambutan, ubah hubungan industri

--- mm.php
+++ mm.php
@@ -150,29 +150,12 @@
     </section><!-- End Portfolio Details Section -->
 
       <!-- ======= Clients Section ======= -->
-      <section id="clients" class="clients">
-        <div class="container" data-aos="zoom-in">
-  
-          <header class="section-header">
-            <h3>Our Clients</h3>
-          </header>
-  
-          <div class="clients-slider swiper">
-            <div class="swiper-wrapper align-items-center">
-              <div class="swiper-slide"><img src="assets/img/clients/client-1.png" class="img-fluid" alt=""></div>
-              <div class="swiper-slide"><img src="assets/img/clients/client-2.png" class="img-fluid" alt=""></div>
-              <div class="swiper-slide"><img src="assets/img/clients/client-3.png" class="img-fluid" alt=""></div>
-              <div class="swiper-slide"><img src="assets/img/clients/client-4.png" class="img-fluid" alt=""></div>
-              <div class="swiper-slide"><img src="assets/img/clients/client-5.png" class="img-fluid" alt=""></div>
-              <div class="swiper-slide"><img src="assets/img/clients/client-6.png" class="img-fluid" alt=""></div>
-              <div class="swiper-slide"><img src="assets/img/clients/client-7.png" class="img-fluid" alt=""></div>
-              <div class="swiper-slide"><img src="assets/img/clients/client-8.png" class="img-fluid" alt=""></div>
-            </div>
-            <div class="swiper-pagination"></div>
-          </div>
-  
-        </div>
-      </section><!-- End Clients Section -->
+    
+       
+    <?php
+     include "sections/klien.php";
+    ?>
+      <!-- ======= End Clients Section ======= -->
   <!-- footer -->
       <?php
       include "footer.php";
--- tkr.php
+++ tkr.php
@@ -151,34 +151,18 @@
     </section><!-- End Portfolio Details Section -->
 
       <!-- ======= Clients Section ======= -->
-      <section id="clients" class="clients">
-        <div class="container" data-aos="zoom-in">
-  
-          <header class="section-header">
-            <h3>Our Clients</h3>
-          </header>
-  
-          <div class="clients-slider swiper">
-            <div class="swiper-wrapper align-items-center">
-              <div class="swiper-slide"><img src="assets/img/clients/client-1.png" class="img-fluid" alt=""></div>
-              <div class="swiper-slide"><img src="assets/img/clients/client-2.png" class="img-fluid" alt=""></div>
-              <div class="swiper-slide"><img src="assets/img/clients/client-3.png" class="img-fluid" alt=""></div>
-              <div class="swiper-slide"><img src="assets/img/clients/client-4.png" class="img-fluid" alt=""></div>
-              <div class="swiper-slide"><img src="assets/img/clients/client-5.png" class="img-fluid" alt=""></div>
-              <div class="swiper-slide"><img src="assets/img/clients/client-6.png" class="img-fluid" alt=""></div>
-              <div class="swiper-slide"><img src="assets/img/clients/client-7.png" class="img-fluid" alt=""></div>
-              <div class="swiper-slide"><img src="assets/img/clients/client-8.png" class="img-fluid" alt=""></div>
-            </div>
-            <div class="swiper-pagination"></div>
-          </div>
-  
-        </div>
-      </section><!-- End Clients Section -->
-  <!-- footer -->
+    
+       
+    <?php
+     include "sections/klien.php";
+    ?>
+      <!-- ======= End Clients Section ======= -->
+ 
+      <!-- footer Section -->
       <?php
       include "footer.php";
       ?>
-  <!-- footer end -->
+  <!-- Section footer end -->
 
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
