b039ff5479644c1e6d7c5084193b426306d9ef5e
Moch. Iqbal Firmansyah <iqbalfirmansyah2211@gmail.com>
2024-03-30 16:59:56 +0700

    update footer

--- footer.php
+++ footer.php
@@ -1,78 +1,61 @@
-<footer id="footer" class="section-bg">
-    <div class="footer-top">
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-6">
-            <div class="row">
-              <div class="col-sm-2">
-                  <img class="img-fluid" src="assets/img/logo.png" alt= "">
-              </div>
-              <div class="col-sm-10">  
-                <div class="footer-info">
-                  <h3>SMK ASY-SYAMSURIYYAH</h3>
-                  <h4>"Bertaqwa, Berkarakter, dan Mandiri"</h4>
-                </div>
-                <!-- <div class="footer-newsletter">
-                  <h4>Our Newsletter</h4>
-                  <p>Tamen quem nulla quae legam multos aute sint culpa legam noster magna veniam enim veniam illum dolore legam minim quorum culpa amet magna export quem.</p>
-                  <form action="" method="post">
-                    <input type="email" name="email"><input type="submit" value="Subscribe">
-                  </form>
-                </div> -->
-              </div>
+<style>
+    footer {
+        width: 100%;
+        padding: 20px;
+    }
 
-              <div style="margin-left: 110px;" class="col-sm-5">
-                <div class="footer-links">
-                  <h4>Menu Utama</h4>
-                  <ul>
-                    <li><a href="#">Beranda</a></li>
-                    <li><a href="#about">Tentang Kami</a></li>
-                    <li><a href="#">Program</a></li>
-                    <li><a href="#">Alumni</a></li>
-                    <li><a href="#">Hubungi Kami</a></li>
-                  </ul>
-                </div>
-              </div>
-              <div class="col-sm-5">
-                <div class="footer-links">
-                  <h4>Hubungi Kami</h4>
-                  <p>
-                    Lempeni, Jagalempeni<br>
-                    Kec. Wanasari, Kab. Brebes<br>
-                    Jawa Tengah<br>
-                    <strong>Phone:</strong> +62 815 5588 8344<br>
-                    <strong>Email:</strong> info@example.com<br>
-                  </p>
-                </div>
+    footer section,
+    footer section .container {
+        width: 100%;
+    }
 
-                <div class="social-links">
-                  <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
-                  <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
-                  <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
-                  <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a>
+    footer section .container .row {
+        display: flex;
+        flex-wrap: wrap;
+    }
+
+    footer section .container .row .col-md-4 {
+        flex: 0 0 auto;
+        width: calc(100% / 3); 
+        max-width: none;
+    }
+
+    footer section .container .row .col-md-4:nth-child(2) {
+        margin-bottom: 0; 
+    }
+</style>
+<footer class="text-center text-lg-start text-white" style="background-color: #1c2331">
+  
+    <section>
+        <div class="container text-center text-md-start mt-5">
+            <div class="row mt-3">
+                <div class="col-md-4 mx-auto mb-4">
+                    <h6 class="text-uppercase fw-bold">Company name</h6>
+                    <hr class="mb-4 mt-0 d-inline-block mx-auto" style="width: 60px; background-color: #7c4dff; height: 2px">
+                    <p>Here you can use rows and columns to organize your footer content. Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
                 </div>
-
-              </div>
-
+                <div class="col-md-4 mx-auto mb-md-0 mb-4">
+                    <h6 class="text-uppercase fw-bold">Contact</h6>
+                    <hr class="mb-4 mt-0 d-inline-block mx-auto" style="width: 60px; background-color: #7c4dff; height: 2px">
+                    <p><i class="fas fa-home mr-3"></i> New York, NY 10012, US</p>
+                    <p><i class="fas fa-envelope mr-3"></i> info@example.com</p>
+                    <p><i class="fas fa-phone mr-3"></i> + 01 234 567 88</p>
+                    <p><i class="fas fa-print mr-3"></i> + 01 234 567 89</p>
+                </div>
+                <div class="col-md-4 mx-auto mb-md-0 mb-4">
+                    <h6 class="text-uppercase fw-bold">Contact</h6>
+                    <hr class="mb-4 mt-0 d-inline-block mx-auto" style="width: 60px; background-color: #7c4dff; height: 2px">
+                    <p><i class="fas fa-home mr-3"></i> New York, NY 10012, US</p>
+                    <p><i class="fas fa-envelope mr-3"></i> info@example.com</p>
+                    <p><i class="fas fa-phone mr-3"></i> + 01 234 567 88</p>
+                    <p><i class="fas fa-print mr-3"></i> + 01 234 567 89</p>
+                </div>
             </div>
-
-          </div>
-
-          <div class="col-lg-6">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3960.5923100716936!2d109.00992827509046!3d-6.93923026792911!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e6fbb467f961021%3A0xfa9b8da11e755086!2sSMK%20As-Syamsuriyyah!5e0!3m2!1sid!2sid!4v1710861117389!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-          </div>
-
-          </div>
-
         </div>
+    </section>
 
-      </div>
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
+        © 2024 Copyright:
+        <a class="text-white">PPM 12 PHB</a>
     </div>
-    <div class="container">
-      <div class="copyright">
-      Copyright &copy;2024 <strong>PPM 12 PHB</strong>. All Rights Reserved.
-      </div>
-      <div class="credits">
-      </div>
-    </div>
-  </footer>
+</footer>
