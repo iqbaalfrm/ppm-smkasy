f07545c99115633d8a719c73265b0720726187de
Humam <humamasathin234@gmail.com>
2024-04-15 12:20:45 +0700

    ganti logo header

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <header id="header" class="fixed-top d-flex align-items-center header-transparent">
     <div class="container d-flex align-items-center">
       
-      <h1 class="logo me-auto"><a href="index.php"><img src="assets/img/logo.png" alt="" class="img-fluid"> SMK ASY-SYAMSURIYYAH</a></h1>
+      <h1 class="logo me-auto"><a href="index.php"><img src="assets/img/humam2.png" alt="" class="img-fluid"></a></h1>
       <!-- Uncomment below if you prefer to use an image logo -->
       <!-- <a href="index.html" class="logo me-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
       <nav id="navbar" class="navbar order-last order-lg-0">
