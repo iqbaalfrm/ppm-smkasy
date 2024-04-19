132536341e2a1c4d183b3b39e77a76a142c6b0e9
Moch. Iqbal Firmansyah <iqbalfirmansyah2211@gmail.com>
2024-04-19 18:36:52 +0700

    feat:added added ppdb page

---
+++ ppdb.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>SMK ASY-SYAMSURIYYAH
+  </title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Montserrat:300,400,500,600,700" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+  <?php include('dependensi.php');?>
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <?php
+     include "header.php";
+    ?>
+  <!-- End Header -->
+
+  <main id="main">
+
+    <!-- ======= Breadcrumbs ======= -->
+    <section id="breadcrumbs" class="breadcrumbs">
+      <div class="container">
+
+        <ol>
+          <li><a href="index.php">Beranda</a></li>
+          <li>Tentang DKV</li>
+        </ol>
+        <h2>Desain Komunikasi Visual</h2>
+
+      </div>
+    </section><!-- End Breadcrumbs -->
+
+    <!-- ======= Portfolio Details Section ======= -->
+  
+    <!-- Menampilkan foto ppdb.jpeg dengan ukuran yang lebih kecil -->
+<!-- Menampilkan foto ppdb.jpeg dengan ukuran yang lebih kecil dan terpusat -->
+<div class="row justify-content-center">
+  <div class="col-lg-8">
+    <div class="card text-center">
+      <img src="assets\img\ppdb.jpeg" class="card-img-top mx-auto" alt="PPDB" style="max-width: 400px; height: auto;">
+      <div class="card-body">
+        <h5 class="card-title">Tulisan Dummy</h5>
+        <p class="card-text">Deskripsi tulisan dummy di bawah gambar.</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+    
+       <!-- End Portfolio Details Section -->
+
+      <!-- ======= Clients Section ======= -->
+    
+       
+    <?php
+     include "sections/klien.php";
+    ?>
+      <!-- ======= End Clients Section ======= -->
+  <!-- footer -->
+      <?php
+      include "footer.php";
+      ?>
+  <!-- footer end -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
